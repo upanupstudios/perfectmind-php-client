905eebdb8d4fbe78658afe7ed4c7c02fea91f09e
Marco Maranao <marco@maranao.ca>
2021-11-03 12:36:28 -0600

    Add properties

--- src/Config.php
+++ src/Config.php
@@ -4,15 +4,35 @@ namespace Upanupstudios\Perfectmind\Php\Client;
 
 final class Config
 {
+  private $apiUrl;
+  private $clientNumber;
   private $apiKey;
 
-  public function __construct(string $apiKey)
+  public function __construct(string $apiUrl, string $clientNumber, string $apiKey)
   {
+    $this->apiUrl = $apiUrl;
+    $this->clientNumber = $clientNumber;
     $this->apiKey = $apiKey;
   }
 
   /**
-   * Get API token.
+   * Get API URL.
+   */
+  public function getApiUrl(): string
+  {
+      return $this->apiUrl;
+  }
+
+  /**
+   * Get Client Number.
+   */
+  public function getClientNumber(): string
+  {
+      return $this->apiKey;
+  }
+
+  /**
+   * Get API Key.
    */
   public function getApiKey(): string
   {
