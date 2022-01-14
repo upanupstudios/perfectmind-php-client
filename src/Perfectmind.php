756e1ad27313e6d8a01d257b30f13c8af539fadd
marcomaranao <marco@maranao.ca>
2022-01-14 16:43:38 -0700

    Use guzzle instead

--- src/Perfectmind.php
+++ src/Perfectmind.php
@@ -2,26 +2,21 @@
 
 namespace Upanupstudios\Perfectmind\Php\Client;
 
-use Psr\Http\Client\ClientInterface;
-use Psr\Http\Message\RequestFactoryInterface;
-use Psr\Http\Message\StreamFactoryInterface;
-use Psr\Http\Message\RequestInterface;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Psr7;
+use GuzzleHttp\Exception\RequestException;
 
 class Perfectmind
 {
   private $config;
   private $httpClient;
-  private $requestFactory;
-  private $streamFactory;
 
   private $version = '2.0';
 
-  public function __construct(Config $config, ClientInterface $httpClient, RequestFactoryInterface $requestFactory, StreamFactoryInterface $streamFactory)
+  public function __construct(Config $config, ClientInterface $httpClient)
   {
     $this->config = $config;
     $this->httpClient = $httpClient;
-    $this->requestFactory = $requestFactory;
-    $this->streamFactory = $streamFactory;
   }
 
   public function getConfig(): Config
@@ -34,67 +29,49 @@ class Perfectmind
     return $this->httpClient;
   }
 
-  public function getRequestFactory(): RequestFactoryInterface
-  {
-    return $this->requestFactory;
-  }
-
-  public function getStreamFactory(): StreamFactoryInterface
-  {
-    return $this->streamFactory;
-  }
-
-  public function sendRequest(RequestInterface $request): array
-  {
-    $response = $this->httpClient->sendRequest($request);
-
-    $contentType = $response->getHeader('Content-Type')[0] ?? 'application/json';
-
-    if (!preg_match('/\bjson\b/i', $contentType)) {
-      throw new JsonException("Response content-type is '$contentType' while a JSON-compatible one was expected.");
-    }
-
-    $content = $response->getBody()->__toString();
-
-    if(is_string($content)) {
-      $content = ['status' => $content];
-      $content = json_encode($content);
-    }
-
-    try {
-      $content = json_decode($content, true, 512, JSON_BIGINT_AS_STRING | JSON_THROW_ON_ERROR);
-    } catch (\JsonException $e) {
-      throw new JsonException($e->getMessage(), $e->getCode(), $e);
-    }
-
-    if (!is_array($content)) {
-      throw new JsonException(sprintf('JSON content was expected to decode to an array, %s returned.', gettype($content)));
-    }
-
-    return $content;
-  }
-
   public function status()
   {
-    $request = $this->getRequestFactory()->createRequest('GET', $this->getConfig()->getApiUrl().'/api/'.$this->version.'/Status');
-    $request = $request->withHeader('Content-Type', 'application/json');
-    $request = $request->withHeader('X-Access-Key', $this->getConfig()->getApiKey());
-    $request = $request->withHeader('X-Client-Number', $this->getConfig()->getClientNumber());
+    try {
+      $request = $this->httpClient->request('GET', $this->getConfig()->getApiUrl().'/api/'.$this->version.'/Status', [
+        'headers' => [
+          'Accept' => 'application/json',
+          'Content-Type' => 'application/json',
+          'X-Client-Number' => $this->getConfig()->getClientNumber(),
+          'X-Access-Key' => $this->getConfig()->getApiKey()
+        ]
+      ]);
 
-    return $this->sendRequest($request);
+      $response = $request->getBody();
+      $response = $response->__toString();
+    } catch (RequestException $exception) {
+      $response = $exception->getMessage();
+    }
+
+    return $response;
   }
 
   public function query(string $query)
   {
-    $body = json_encode(array('QueryString' => $query));
+    try {
+      $body = Psr7\Utils::streamFor(json_encode(array('QueryString' => $query)));
 
-    $request = $this->getRequestFactory()->createRequest('POST', $this->getConfig()->getApiUrl().'/api/'.$this->version.'/B2C/Query');
-    $request = $request->withHeader('Content-Type', 'application/json');
-    $request = $request->withHeader('X-Client-Number', $this->getConfig()->getClientNumber());
-    $request = $request->withHeader('X-Access-Key', $this->getConfig()->getApiKey());
-    $request = $request->withBody($this->getStreamFactory()->createStream($body));
+      $request = $this->httpClient->request('POST', $this->getConfig()->getApiUrl().'/api/'.$this->version.'/B2C/Query', [
+        'headers' => [
+          'Accept' => 'application/json',
+          'Content-Type' => 'application/json',
+          'X-Client-Number' => $this->getConfig()->getClientNumber(),
+          'X-Access-Key' => $this->getConfig()->getApiKey(),
+        ],
+        'body' => $body
+      ]);
 
-    return $this->sendRequest($request);
+      $response = $request->getBody();
+      $response = $response->__toString();
+    } catch (RequestException $exception) {
+      $response = $exception->getMessage();
+    }
+
+    return $response;
   }
 
   /**
@@ -108,7 +85,7 @@ class Perfectmind
 
     switch ($name) {
       default:
-        throw new InvalidArgumentException("Undefined api instance called: '$name'.");
+        throw new \InvalidArgumentException("Undefined api instance called: '$name'.");
     }
 
     return $api;
@@ -118,8 +95,8 @@ class Perfectmind
   {
     try {
       return $this->api($name);
-    } catch (InvalidArgumentException $e) {
-      throw new BadMethodCallException("Undefined method called: '$name'.");
+    } catch (\InvalidArgumentException $e) {
+      throw new \BadMethodCallException("Undefined method called: '$name'.");
     }
   }
 }
